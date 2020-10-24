8c6f33c5cf23f6bb90b1ad35f9a0d8e38ac66597
AnthonyStembreit <stembreit@gmail.com>
2020-10-24 15:27:25 -0500

    Updates

---
+++ precache-manifest.add2db0ef69335b459f51f44ccbeeb06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ab12610ad3359a485fba0543567129",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "75e1681d6c777519fb87",
+    "url": "/React-Employee-Directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "75e1681d6c777519fb87",
+    "url": "/React-Employee-Directory/static/js/2.4e8109e3.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/React-Employee-Directory/static/js/2.4e8109e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bbe81340293c067b391",
+    "url": "/React-Employee-Directory/static/js/main.88bc6499.chunk.js"
+  },
+  {
+    "revision": "fd74d32e2fdcce7b26d8",
+    "url": "/React-Employee-Directory/static/js/runtime-main.021b6c8f.js"
+  }
+]);
